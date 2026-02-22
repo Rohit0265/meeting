f2d9ebf7b7e1f61028264a5ec0d207394c1b084a
Rohit0265 <rohitmathur05458@gmail.com>
2026-02-22 19:18:26 +0530

    bol

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+
+
+const Meeting =async ({params,}: {params: Promise<{ id: string }>}) => {
+  const { id } = await params;
+  
+  return <div>Meeting Room: #{id}</div>;
+}
+export default Meeting
